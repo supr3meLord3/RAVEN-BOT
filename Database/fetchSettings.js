cce1beb7dbe5c37c5f5c09081810a82aacc27380
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2025-07-15 10:30:46 +0300

    Update fetchSettings.js

--- Database/fetchSettings.js
+++ Database/fetchSettings.js
@@ -16,7 +16,7 @@ async function fetchSettings() {
     antitag: data.antitag,
     antiforeign: data.antiforeign,
     antibot: data.antibot,
-    welcomegoodbye: data.wwelcomegoodbye,
+    welcomegoodbye: data.welcomegoodbye,
     autobio: data.autobio,
     badword: data.badword,
     gptdm: data.gptdm, 
